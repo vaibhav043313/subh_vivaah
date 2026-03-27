7f770374de4e5cc4301be5498859264ae7a87dc9
Vaibhav Kushwaha <vaibhavk@westbridgecapital.ca>
2026-03-28 03:11:12 +0530

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 
   resource :profile, only: [:create, :show]
 end
