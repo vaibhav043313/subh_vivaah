f4186a9b37d0a52e5c83ead0f6dd8fc0f2daacee
Vaibhav Kushwaha <vaibhavk@westbridgecapital.ca>
2026-04-27 03:18:27 +0530

    Photos fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Dockerfile installs libvips; use it for variants (faster than mini_magick).
+  config.active_storage.variant_processor = :vips
+
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # config.assume_ssl = true
 
